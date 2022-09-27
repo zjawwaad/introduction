da0f4d6782b95e57d7675e0159c45a9e9fe24684
zjawwaad <zjawwaad@yahoo.com>
2022-09-27 11:36:12 -0400

    Introductions commit

---
+++ hello_world.py
@@ -0,0 +1,29 @@
+# 1. TASK: print "Hello World"
+greeting="Hello World"
+print(greeting)
+
+
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Zaynah"
+print('Hello ',name)	# with a comma
+print('Hello ' + name) # print( your code here ) # with a +
+
+
+# # 3. print "Hello 42!" with the number in a variable
+number= 7
+print('Hello ' + str(number))
+
+# name = 42
+# print( your code here )	# with a comma
+# print( your code here )	# with a +	-- this one should give us an error!
+print('Hello ', number)
+
+
+# # 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "rasta pasta"
+print(f"I love to eat {fave_food1} and {fave_food2}!")
+
+print("I love to eat {} and {} ! ".format(fave_food1, fave_food2))
+# print( your code here ) # with .format()
+# print( your code here ) 
