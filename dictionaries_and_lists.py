0caae00756ca2818874318de66212df4dad80e5f
zjawwaad <zjawwaad@yahoo.com>
2022-09-27 13:57:39 -0400

    FINAL COMMIT

--- dictionaries_and_lists.py
+++ dictionaries_and_lists.py
@@ -2,7 +2,7 @@
 # Change the last_name of the first student from 'Jordan' to 'Bryant'
 # In the sports_directory, change 'Messi' to 'Andres'
 # Change the value 20 in z to 30x = [ [5,2,3], [10,8,9] ] 
-
+x = [ [5,2,3], [10,8,9] ]
 
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -14,7 +14,7 @@ sports_directory = {
 }
 z = [ {'x': 10, 'y': 20} ]
 
-x [1][0]=15
+x[1][0]=15
 print(x)
 
 students[0]['last_name']="Bryant"
@@ -42,3 +42,33 @@ def iterateDictionary(students):
     for i in range(len(students)):
         print(students[i])
 iterateDictionary(students)
+
+def iterateDictionary2(key_name,students):
+    for i in students:
+        print(i[key_name])
+iterateDictionary2('first_name',students)
+
+# Get Values From a List of Dictionaries
+# Create a function iterateDictionary2(key_name, some_list) 
+# that, given a list of dictionaries and a key name, the function
+#  prints the value stored in that key for each dictionary. For example,
+#   iterateDictionary2('first_name', students) should output:
+
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa',
+    'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 
+    'Patrick', 'Minh', 'Devon']
+}
+
+def iterateCd(key_name,dojo):
+    for i in (dojo[key_name]):
+        print(i)
+iterateCd('locations',dojo)
+
+
+def iterateCd(key_name,dojo):
+    for i in (dojo[key_name]):
+        print(i)
+iterateCd('instructors',dojo)
